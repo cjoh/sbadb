f60e99aeedd4406bb96871583e23fccf1abc1ba0
Jed Wood <jed.wood@limechile.com>
2012-08-21 14:56:10 -0400

    baseline for parsing files and cleaning up booleans

--- foia/controller.js
+++ foia/controller.js
@@ -1,16 +1,47 @@
 var fs = require('fs');
 var Biz = require('./model').Biz;
+
+var convertToBoolean = function(bit) {
+  if (typeof bit == 'undefined') return false;
+  if (bit.toUpperCase() == 'Y') {
+    return true;
+  } else {
+    return false;
+  }
+};
+
 exports.parse = function(req, res) {
-  fs.readFile(__dirname + '/dump/PRO_ID.TXT', 'ascii', function (err,data) {
+  console.log("calling parse");
+  txtToJson(__dirname + '/dump/PRO_ID.TXT', function(json){
+    console.log("Back with the json");
+    var boolProps = ['Gcc'];
+    for (var i=0, leni = json.length; i<leni; i++) {
+      for (var j=0, lenj = boolProps.length; j<lenj; j++) {
+        json[i][boolProps[j]] = convertToBoolean(json[i][boolProps[j]]);
+      }
+    }
+    res.send(json.length + " records loaded");
+  });
+};
+
+var txtToJson = function(filepath, cb) {
+  fs.readFile(filepath, 'ascii', function (err,data) {
     if (err) {
       return console.log("ERROR! -- \n" + err);
     }
     var rows = data.split("\n");
-    var len = rows.length;
-    for (var i=0; i<len; i++) {
-      var row = rows[i];
-      Biz.createFromDump(row);
+    var keys = rows.shift().split('\t');
+    var numkeys = keys.length;
+    var retJson = [];
+    for (var i=0, len = rows.length; i<len; i++) {
+      var vals = rows[i].split('\t');
+      var doc = {};
+      for (var j=0; j<numkeys; j++){
+        doc[keys[j]] = (typeof vals[j] == 'undefined' || vals[j] === '') ? '' : vals[j].replace(/^\s+|\s+$/g,"");
+      }
+      retJson.push(doc);
     }
-    res.send(len + " records loaded");
+    console.log("calling the cb");
+    cb(retJson);
   });
 };
--- foia/model.js
+++ foia/model.js
@@ -1,10 +1,46 @@
 var mongoose = require('mongoose');
-var bizSchema = new mongoose.Schema({
 
+var bizSchema = new mongoose.Schema({
+  User_Id: String,
+  Name: String,
+  Address: String,
+  Address1: String,
+  City: String,
+  Cnty: String,
+  State: String,
+  Zip: String,
+  Phone: Number,
+  Fax: Number,
+  Cdist: String,
+  Msa: String,
+  Duns: String,
+  Cage: String,
+  Yrest: String,
+  Contact: String,
+  Title: String,
+  Url: String,
+  Gcc: {type: Boolean, default: false},
+  Edi: {type: Boolean, default: false},
+  BusParntDUNSNmb: String,
+  ExportCd: {type: Boolean, default: false},
+  ExporctObjtvTxt: String,
+  TechnetInd: String,
+  Emall: String,
+  Tof: String,
+  Minc: String,
+  Women: {type: Boolean, default: false},
+  Veteran: {type: Boolean, default: false},
+  Dav: {type: Boolean, default: false},
+  Vietnam: {type: Boolean, default: false},
+  RgstrtnCCRInd: {type: Boolean, default: false},
+  BusLastUpdtDt: Date,
+  latlon: Array
 });
 
+//bizSchema.index({'latlon':'2d'});
+
 bizSchema.statics.createFromDump = function(row) {
-  //console.log(row);
+
 };
 
-var Biz = exports.Biz = DB.model('Biz', bizSchema);
+exports.Biz = DB.model('Biz', bizSchema);
