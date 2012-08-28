4814899c84eb1dd1f4b188b1d850d95003047952
adamjacobbecker <ad@mbecker.cc>
2012-08-28 14:17:55 -0400

    make param parsing more modular - take one

--- foia/controllers/v1.js
+++ foia/controllers/v1.js
@@ -6,12 +6,11 @@ var booleanProps = ['gcc', 'edi', 'exportcd', 'women', 'veteran', 'dav', 'vietna
 
 exports.index = function(req, res) {
 
-  var searchParams = {}
-    , page = 1;
-
   var convertToBoolean = function (str) {
     if (str.toLowerCase() === "true" ||
         str.toLowerCase() === "t" ||
+        str.toLowerCase() === "yes" ||
+        str.toLowerCase() === "y" ||
         str === "1"){
       return true;
     } else {
@@ -19,66 +18,109 @@ exports.index = function(req, res) {
     }
   }
 
-  // Construct new searchParams object, using lowercase keys
-  // and case-insensitive values.
+  var searchParams = {}
+    , page
+    , per_page = 100
+    , skip
+    , query;
+
+  var parseSchemaForKey = function (schema, keyPrefix, lcKey, val, option) {
+
+    var addSearchParam = function (param) {
+      console.log(param)
+      for (key in param) {
+        searchParams[keyPrefix + key] = param[key];
+      }
+    }
+
+    var param = {};
+
+    if (matches = lcKey.match(/(.+)\.(.+)/)) {
+      // parse subschema
+      if (schema.paths[matches[1]].constructor.name === "DocumentArray") {
+        parseSchemaForKey(schema.paths[matches[1]].schema, matches[1] + ".", matches[2], val, option)
+      }
+
+    } else if (lcKey === "" || typeof schema.paths[lcKey] === "undefined"){
+      // nada
+
+    } else if (schema.paths[lcKey].constructor.name === "SchemaBoolean") {
+      param[lcKey] = convertToBoolean(val);
+
+    } else if (schema.paths[lcKey].constructor.name === "SchemaString") {
+
+      if (val.match(/([0-9]+,?)/)) {
+        if (option === "all") {
+          param[lcKey] = {$all: val.split(',')};
+        } else {
+          param[lcKey] = {$in: val.split(',')};
+        }
+      } else if (val.match(/([0-9]+)/)) {
+        if (option === "gt" ||
+            option === "gte" ||
+            option === "lt" ||
+            option === "lte") {
+          param[lcKey] = {};
+          param[lcKey]["$" + option] = val;
+        } else {
+          param[lcKey] = val;
+        }
+      } else {
+        param[lcKey] = {$regex: val, $options: "-i"};
+      }
+
+
+    } else if (schema.paths[lcKey].constructor.name === "SchemaNumber") {
+
+      if (val.match(/([0-9]+,?)/)) {
+        if (option === "all") {
+          param[lcKey] = {$all: val.split(',')};
+        } else {
+          param[lcKey] = {$in: val.split(',')};
+        }
+      } else if (val.match(/([0-9]+)/)) {
+        if (option === "gt" ||
+            option === "gte" ||
+            option === "lt" ||
+            option === "lte") {
+          param[lcKey] = {};
+          param[lcKey]["$" + option] = val;
+        } else {
+          param[lcKey] = val;
+        }
+      }
+    }
+
+    if (param) addSearchParam(param);
+  }
+
+
+  // Construct searchParams
   for (key in req.query) {
     var lcKey = key.toLowerCase()
       , val = req.query[key]
-      , matches
       , option;
 
-    if (matches = val.match(/{([a-z]+)}/i)) {
-      option = matches[1].toLowerCase();
-      val = val.replace(/{([a-z]+)}/i, '');
-    }
-
-    if (val == "") {
-      searchParams[lcKey] = "";
-
-    } else if (lcKey == 'page'){
+    if (lcKey === "page") {
       page = parseInt(val);
 
-    } else if (lcKey == 'near') {
+    } else if (lcKey === "near") {
       // divide by 69 to convert miles to degrees
       // default to 5 miles
       var maxDistance = (typeof req.query['radius'] === 'undefined' ? 5 : parseFloat(req.query['radius'])) / 69;
       var latlng = val.split(',');
       searchParams["latlon"] = {$near: [parseFloat(latlng[0]), parseFloat(latlng[1])], $maxDistance: maxDistance};
 
-    } else if (lcKey == 'radius') {
-      // see above
-
-    } else if (booleanProps.indexOf(lcKey) !== -1) {
-      searchParams[lcKey] = convertToBoolean(val);
-
-    } else if (!isNaN(val)) {
-      if (option === "gt" ||
-          option === "gte" ||
-          option === "lt" ||
-          option === "lte") {
-        searchParams[lcKey] = {};
-        searchParams[lcKey]["$" + option] = val;
-      } else {
-        searchParams[lcKey] = val;
-      }
-
-    } else if (val.match(/([0-9]+,?)/)) {
-      if (option === "all") {
-        searchParams[lcKey] = {$all: val.split(',')};
-      } else {
-        searchParams[lcKey] = {$in: val.split(',')};
-      }
     } else {
-      searchParams[lcKey] = {$regex: val, $options: "-i"};
+      parseSchemaForKey(Biz.schema, "", lcKey, val, option);
     }
   }
 
   // Query the database and return the results as JSON.
-  var per_page = 100,
-      skip = (page - 1) * per_page,
-      query = Biz.find(searchParams).skip(skip).limit(per_page);
+  skip = (page - 1) * per_page,
+  query = Biz.find(searchParams).skip(skip).limit(per_page);
 
-      console.log(searchParams)
+  console.log(searchParams)
 
   query.exec(function (err, results) {
 
@@ -95,4 +137,5 @@ exports.index = function(req, res) {
     });
 
   });
+
 };
