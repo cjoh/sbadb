d051475f5ab4d94df14b3f05d4823b11cfcc4500
adamjacobbecker <ad@mbecker.cc>
2012-08-28 15:00:07 -0400

    v1 of mongoose-api-query

--- foia/controllers/v1.js
+++ foia/controllers/v1.js
@@ -1,144 +1,41 @@
 var fs = require('fs');
 var Biz = require('../model').Biz;
+var mongooseApiQuery = require('../../mongoose-api-query');
 
 var booleanProps = ['gcc', 'edi', 'exportcd', 'women', 'veteran', 'dav', 'vietnam', 'rgstrtnccrind',
                     'naics.naicsprimind', 'naics.naicsgreenind', 'naics.naicssmllbusind', 'naics.naicsemrgsmllbusind'];
 
 exports.index = function(req, res) {
 
-  var convertToBoolean = function (str) {
-    if (str.toLowerCase() === "true" ||
-        str.toLowerCase() === "t" ||
-        str.toLowerCase() === "yes" ||
-        str.toLowerCase() === "y" ||
-        str === "1"){
-      return true;
-    } else {
-      return false;
-    }
-  }
-
-  var searchParams = {}
-    , page
-    , per_page = 100
-    , skip
-    , query;
-
-  var parseSchemaForKey = function (schema, keyPrefix, lcKey, val, option) {
-
-    var addSearchParam = function (param) {
-      for (key in param) {
-        searchParams[keyPrefix + key] = param[key];
+  mongooseApiQuery(req, {
+    custom_params: function(key, val, searchParams) {
+      if (key === "near") {
+        // divide by 69 to convert miles to degrees
+        // default to 5 miles
+        var maxDistance = (typeof req.query['radius'] === 'undefined' ? 5 : parseFloat(req.query['radius'])) / 69;
+        var latlng = val.split(',');
+        searchParams["latlon"] = {$near: [parseFloat(latlng[0]), parseFloat(latlng[1])], $maxDistance: maxDistance};
+        return true;
       }
-    }
+    },
+    model: Biz,
+    per_page: 100
+  }, function(query, attributes){
 
-    var param = {};
+    query.exec(function (err, results) {
+      if (err) return console.log(err);
 
-    if (matches = lcKey.match(/(.+)\.(.+)/)) {
-      // parse subschema
-      if (schema.paths[matches[1]].constructor.name === "DocumentArray") {
-        parseSchemaForKey(schema.paths[matches[1]].schema, matches[1] + ".", matches[2], val, option)
-      }
+      var response = {};
+      response.results = results;
+      response.meta = {page: attributes.page, per_page: attributes.per_page }
 
-    } else if (lcKey === "" || typeof schema.paths[lcKey] === "undefined"){
-      // nada
+      Biz.count(attributes.searchParams, function(err, count){
+        response.meta.count = count;
+        response.meta.total_pages = Math.ceil(count / attributes.per_page);
+        res.send(response);
+      });
 
-    } else if (schema.paths[lcKey].constructor.name === "SchemaBoolean") {
-      param[lcKey] = convertToBoolean(val);
-
-    } else if (schema.paths[lcKey].constructor.name === "SchemaString") {
-
-      if (val.match(/([0-9]+,?)/) && val.match(',')) {
-        if (option === "all") {
-          param[lcKey] = {$all: val.split(',')};
-        } else {
-          param[lcKey] = {$in: val.split(',')};
-        }
-      } else if (val.match(/([0-9]+)/)) {
-        if (option === "gt" ||
-            option === "gte" ||
-            option === "lt" ||
-            option === "lte") {
-          param[lcKey] = {};
-          param[lcKey]["$" + option] = val;
-        } else {
-          param[lcKey] = val;
-        }
-      } else {
-        param[lcKey] = {$regex: val, $options: "-i"};
-      }
-
-
-    } else if (schema.paths[lcKey].constructor.name === "SchemaNumber") {
-
-      if (val.match(/([0-9]+,?)/) && val.match(',')) {
-        if (option === "all") {
-          param[lcKey] = {$all: val.split(',')};
-        } else {
-          param[lcKey] = {$in: val.split(',')};
-        }
-      } else if (val.match(/([0-9]+)/)) {
-        if (option === "gt" ||
-            option === "gte" ||
-            option === "lt" ||
-            option === "lte") {
-          param[lcKey] = {};
-          param[lcKey]["$" + option] = val;
-        } else {
-          param[lcKey] = val;
-        }
-      }
-    }
-
-    if (param) addSearchParam(param);
-  }
-
-
-  // Construct searchParams
-  for (key in req.query) {
-    var lcKey = key.toLowerCase()
-      , val = req.query[key].replace(/{(.*)}/, '')
-      , option;
-
-    if (matches = req.query[key].match(/{(.*)}/)){
-      option = matches[1];
-    }
-
-    if (lcKey === "page") {
-      page = parseInt(val);
-
-    } else if (lcKey === "near") {
-      // divide by 69 to convert miles to degrees
-      // default to 5 miles
-      var maxDistance = (typeof req.query['radius'] === 'undefined' ? 5 : parseFloat(req.query['radius'])) / 69;
-      var latlng = val.split(',');
-      searchParams["latlon"] = {$near: [parseFloat(latlng[0]), parseFloat(latlng[1])], $maxDistance: maxDistance};
-
-    } else {
-      parseSchemaForKey(Biz.schema, "", lcKey, val, option);
-    }
-  }
-
-  // Query the database and return the results as JSON.
-  skip = (page - 1) * per_page,
-  query = Biz.find(searchParams).skip(skip).limit(per_page);
-
-  console.log(searchParams)
-
-  query.exec(function (err, results) {
-
-    if (err) return console.log(err);
-
-    var response = {};
-    response.results = results;
-    response.meta = {page: page, per_page: per_page }
-
-    Biz.count(searchParams, function(err, count){
-      response.meta.count = count;
-      response.meta.total_pages = Math.ceil(count / per_page);
-      res.send(response);
     });
-
   });
 
 };
