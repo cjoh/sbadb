46ec4e1492bdb67265164a59a9acf52eeea078d1
adamjacobbecker <ad@mbecker.cc>
2012-08-29 15:58:58 -0400

    allow CORS

--- foia/controllers/v1.js
+++ foia/controllers/v1.js
@@ -7,6 +7,11 @@ var booleanProps = ['gcc', 'edi', 'exportcd', 'women', 'veteran', 'dav', 'vietna
 
 exports.index = function(req, res) {
 
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
   mongooseApiQuery(req.query, {
     custom_params: function(key, val, searchParams) {
       if (key === "ne_lat") {
